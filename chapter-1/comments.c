8baf722a97c36cf9da79c4b10c97aafee54119e9
dis-craft <mrsrikart@gmail.com>
2024-12-09 20:37:45 +0530

    update

--- chapter-1/comments.c
+++ chapter-1/comments.c
@@ -3,5 +3,10 @@ int main(){
     // This is a comment!
     printf("Hello World\n");
     return 0;
+    /*
+        this is multi line comment!
+        this works!
+        it sure does!
+    */
 }
   
