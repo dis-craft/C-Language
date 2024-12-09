0d3c65d70e407cec91973d038f93edb5a7bd8ca2
dis-craft <mrsrikart@gmail.com>
2024-12-09 19:09:21 +0530

    chapter 1

---
+++ chapter-1/variable.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a; //Var declaration
+    a=6; //Var initialization
+    printf("%d\n", a);
+    printf("The output of this program is: %d", a);
+    return 0;
+}
---
+++ chapter-1/variables_rules.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a; //Var declaration
+    // int 1; - not allowed
+    // int hello$; - not allowed
+    // int %hello; -not allowed
+    int _hello; //allowed
+    int A; //c is case sensitive hence it is diff from a
+    return 0;
+}
