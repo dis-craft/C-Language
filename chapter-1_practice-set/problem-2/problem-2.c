5c69f481c7d56b22b621ec074f3aeb0799ad9420
dis-craft <mrsrikart@gmail.com>
2024-12-10 17:48:22 +0530

    update

--- chapter-1_practice-set/problem-2/problem-2.c
+++ chapter-1_practice-set/problem-2/problem-2.c
@@ -5,11 +5,11 @@ Calculate the area of circle and cylinder
 int main(){
     float radius, area_c, h, area_cyl;
     printf("Enter the value of height");
-    scanf("%f", h);
+    scanf("%f", &h); //donot forget &!!!
     printf("Enter the value of radius");
-    scanf("%f", radius);
-    area_cyl=area_c*h;
+    scanf("%f", &radius);
     area_c=3.14*radius*radius;
+    area_cyl=area_c*h;
     printf("The area of the circle is : %f", area_c);
     printf("The area of the cylinder is : %f", area_cyl);
     return 0;
