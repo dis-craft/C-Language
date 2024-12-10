eaf5d4003bff2f4e29b841c55d1c98131c510b1b
dis-craft <mrsrikart@gmail.com>
2024-12-10 18:55:25 +0530

    update

---
+++ chapter-2/arithmetic_operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a=4,b=3,c;
+    c=a+b;
+    printf("The value of c is: %d", c);
+    //modulus operator is used to get remainder     
+    printf("The value of a remainder b is: %d",  a%b);
+    return 0;
+}
---
+++ chapter-2/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    i=10;
+    j=i;
+    printf("The value of j is %d , \ni is %d ", j,i); 
+    return 0;
+}
