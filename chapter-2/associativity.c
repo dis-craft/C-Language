4df32f7213f9633326d5416a9cfccbf16cefb76e
dis-craft <mrsrikart@gmail.com>
2024-12-11 17:58:45 +0530

    added associativity.c

---
+++ chapter-2/associativity.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a =3;
+    int b =6;
+    int c =9;
+    printf("The vlaue is: %d \n", a*b/3*c);
+    // 18/3*c
+    // 6*c
+    // 6*9
+    // 54
+    return 0;
+}
