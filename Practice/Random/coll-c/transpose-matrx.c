8f7ba6f368f900417ef87e3862d181fc9e36061d
dis-craft <mrsrikart@gmail.com>
2024-12-16 22:50:48 +0530

    transpose of matrix

--- Practice/Random/coll-c/transpose-matrx.c
+++ Practice/Random/coll-c/transpose-matrx.c
@@ -8,9 +8,19 @@ void readarr(int arr[10][10], int m, int n){
         }
     }
 }
-void writearr(int arr[10][10], int m, int n){
+void writearr(int arr[10][10], int m, int n, int x){
+    if(x==0){
+    printf("The normal matrix is:\n"); 
+    for(int i=0; i<m;i++){ 
+        printf("\n");
+        for(int j=0; j<n; j++){
+            printf("\t%d", arr[i][j]); //normal
+        }
+    }
+    }
+    else{
+    printf("\n The transposed matrix is:\n"); 
     int transpose[10][10];
-            printf("The transposed matrix is:\n"); //transpose
     for(int i=0; i<m;i++){ 
         printf("\n");
         for(int j=0; j<n; j++){
@@ -18,6 +28,8 @@ void writearr(int arr[10][10], int m, int n){
             printf("\t%d", transpose[i][j]); //transpose
         }
     }
+    }
+    
 }
 
 
@@ -29,6 +41,7 @@ int main() {
     printf("Enter the dimensions of the arr: ");
     scanf("%d%d", &m, &n);
     readarr(arr, m, n);
-    writearr(arr, m, n);
+    writearr(arr, m, n, 0);
+    writearr(arr, m, n, 1);
     return 0;
 }
