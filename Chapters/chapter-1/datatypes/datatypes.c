5e39ca5694e6e2a33b958ba146ecc1b4fcdef750
dis-craft <mrsrikart@gmail.com>
2024-12-22 11:37:36 +0530

    revise

--- Chapters/chapter-1/datatypes/datatypes.c
+++ Chapters/chapter-1/datatypes/datatypes.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
     int a=1; //memory allocation 4bytes or 32bits
-    float b=1.4; //memory allocation 1byte or 8bits
-    char c='A'; //memory allocation 4bytes or 32bits
+    float b=1.4; //memory allocation 4bytes or 32bits
+    char c='A'; //memory allocation 1byte or 8bits
     // not allowed char c="A"; only single quotes
     printf("%c\n", c);
     printf("%d\n", a);
     printf("%f\n", b);
     return 0;
 }
+
