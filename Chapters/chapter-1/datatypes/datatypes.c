106eb5af21b7a4406c443aae70c539ad91c560d3
dis-craft <mrsrikart@gmail.com>
2024-12-27 18:03:44 +0530

    added float to wihlitc

--- Chapters/chapter-1/datatypes/datatypes.c
+++ Chapters/chapter-1/datatypes/datatypes.c
@@ -3,7 +3,7 @@ int main(){
     int a=1; //memory allocation 4bytes or 32bits
     float b=1.4; //memory allocation 4bytes or 32bits
     char c='A'; //memory allocation 1byte or 8bits
-    // not allowed char c="A"; only single quotes
+    // not allowed char c="A"; only single quotes for a single char, for strings it is "".
     printf("%c\n", c);
     printf("%d\n", a);
     printf("%f\n", b);
