39c1a20be0a60a38b53a1ca685fd1b307d816f74
dis-craft <mrsrikart@gmail.com>
2024-12-28 16:58:19 +0530

    added logical operators

--- Chapters/chapter-3/logical_operator/logical_operator.c
+++ Chapters/chapter-3/logical_operator/logical_operator.c
@@ -1,32 +1,36 @@
+//logical AND , OR
+
 #include <stdio.h>
 
+void calculate() {
+    int a,b;
+    printf("Enter the value of a, b \n");
+    scanf("%d %d", &a, &b);
+    printf("The value of a AND b is: %d \n", a&&b); // && is AND operator, takes a boolean value that is 1-true, 0-false
+    printf("The value of a OR b is: %d \n", a||b); // || is OR operator
+    printf("The value of not(a) is: %d \n", !a); // || is OR operator
+    printf("The value of not(b) is: %d \n", !b); // || is OR operator
+    if(a && b){ // if both are true, then the if statement gets a value of 1, else 0-meaning the condition is false
+        printf("Both a and b are true \n");
+    }
+    else if(a || b){ // this condition is true when either of a, b is true, if true returns a value of 1, else 0-meaning the condition is false
+        printf("Either a or b is true \n");
+    }
+    else{ // if both are false, then this executes!
+        printf("Both a and b are false \n");
+    }
+}
+
 int main() {
-    int m, n, i, c, arr[10];
-    printf("Enter the value of m\n");
-    scanf("%d", &m);
-
-    printf("Enter the value of arr\n");
-    for(c = 0; c < m; c++) {
-        scanf("%d", &arr[c]);
+    calculate();
+    int n;
+    printf("Do you want to run again? (1/0) \n");
+    scanf("%d", &n);
+    if(n==1){
+        main();
     }
-
-    // Bubble Sort
-    for(i = 0; i < m - 1; i++) {
-        for(c = 0; c < m - i - 1; c++) {
-            if(arr[c] > arr[c + 1]) {
-                // Swap the elements
-                int temp = arr[c];
-                arr[c] = arr[c + 1];
-                arr[c + 1] = temp;
-            }
-        }
+    else{
+        printf("Thank you for using the program \n");
     }
-
-    printf("The sorted elements are: \n");
-
-    for(c = 0; c < m; c++) {
-        printf("\t %d", arr[c]);
-    }
-
     return 0;
 }
