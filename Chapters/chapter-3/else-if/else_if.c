85dac8f22e8b38b8106bd073aac33e44c7d62911
dis-craft <mrsrikart@gmail.com>
2024-12-28 17:50:26 +0530

    else-if ladder

--- Chapters/chapter-3/else-if/else_if.c
+++ Chapters/chapter-3/else-if/else_if.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-        
-    return 0;
+    int age;
+    printf("Enter your age: \n");
+    scanf("%d", &age);
+    if (age > 18 && age < 60) {
+        printf("you can drive");
+    }
+    else if (age == 18) {
+        printf("Congrats you are eighteen now, you can apply for DL");    
+    }
+    else{
+        printf("you cannot drive");
+    }
+
 }
