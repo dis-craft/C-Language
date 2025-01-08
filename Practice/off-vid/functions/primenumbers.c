74d55e32e7df9e32cf317a5ac8718b7b24b4f19c
dis-craft <mrsrikart@gmail.com>
2025-01-08 23:28:07 +0530

    minor changes

---
+++ Practice/off-vid/functions/primenumbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, max_range, prime_count = 0;
+
+    printf("Enter the max range!\n");
+    scanf("%d", &max_range);
+
+    for (j = 2; j <= max_range; j++) { // Iterate through each number in the range
+        int is_prime = 1; // Assume the number is prime
+        for (i = 2; i < j; i++) { // Check divisors from 2 to j-1
+            if (j % i == 0) {
+                is_prime = 0; // Not a prime number
+                break;
+            }
+        }
+        if (is_prime) {
+            prime_count++; // Increment prime count
+        }
+    }
+
+    printf("The number of prime numbers from 2 to %d is: %d\n", max_range, prime_count);
+
+    return 0;
+}
