059def3f5bee3368d49753743c1489e6e4e30ec6
dis-craft <mrsrikart@gmail.com>
2025-01-25 13:38:53 +0530

    added 3

--- Practice/off-vid/c_college/1_10_programs/program3.c
+++ Practice/off-vid/c_college/1_10_programs/program3.c
@@ -1,2 +1,23 @@
 /*C program to check the given character is 
 lowercase or uppercase or special character*/
+
+#include <stdio.h>
+
+int main() {
+    char c;
+
+    printf("Enter any character: ");
+    scanf("%c", &c);
+
+    if (c >= 'A' && c <= 'Z') {
+        printf("character is an upper case\n");
+    } else if (c >= 'a' && c <= 'z') {
+        printf("character is a lower case\n");
+    } else if (c >= '0' && c <= '9') {
+        printf("it is not a character\n");
+    } else {
+        printf("character is a special character\n");
+    }
+
+    return 0;
+}
--- Practice/off-vid/c_college/1_10_programs/program4.c
+++ Practice/off-vid/c_college/1_10_programs/program4.c
@@ -1,3 +1,4 @@
-/*Program to balace the given chemical equation values x,y,p,q  of a simple
+/*Program to balance the given chemical equation values x,y,p,q  of a simple
 chemical equation of the type: The task is to find the values of constants b1,b2,b3
 such that the equation is balanced on both side and it must be the reduced form.*/
+
