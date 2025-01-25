716e674ceff6d99122b728edb58be463a42a2302
dis-craft <mrsrikart@gmail.com>
2025-01-25 13:39:49 +0530

    added 2

--- Practice/off-vid/c_college/1_10_programs/program2.c
+++ Practice/off-vid/c_college/1_10_programs/program2.c
@@ -1,1 +1,20 @@
 /*C program to convert kilometer into meter, centimeter and milimeter*/
+
+#include <stdio.h>
+
+int main() {
+    float km, m, cm, mm;
+
+    printf("Enter the distance in Kilometer: ");
+    scanf("%f", &km);
+
+    m = km * 1000.0;
+    cm = km * 100000.0;
+    mm = km * 1000000.0;
+
+    printf("Distance in meter is %.6f\n", m);
+    printf("Distance in centimeter is %.6f\n", cm);
+    printf("Distance in millimeter is %.6f\n", mm);
+
+    return 0;
+}
